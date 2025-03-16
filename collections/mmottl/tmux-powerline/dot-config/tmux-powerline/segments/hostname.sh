65802b91c7e3db5f7e3ccea5f1b58e60c3079936
Markus Mottl <markus.mottl@gmail.com>
2025-03-16 00:13:14 -0400

    Added more packages

---
+++ collections/mmottl/tmux-powerline/dot-config/tmux-powerline/segments/hostname.sh
@@ -0,0 +1,29 @@
+# shellcheck shell=bash
+# Prints the hostname.
+
+TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT:-short}"
+
+generate_segmentrc() {
+  read -r -d '' rccontents <<EORC
+# Use short or long format for the hostname. Can be {"short, long"}.
+export TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT}"
+EORC
+  echo "$rccontents"
+}
+
+run_segment() {
+  if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
+    return 0
+  fi
+  local opts=""
+  if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
+    if shell_is_osx || shell_is_bsd; then
+      opts="-s"
+    else
+      opts="--short"
+    fi
+  fi
+
+  hostname ${opts}
+  return 0
+}
