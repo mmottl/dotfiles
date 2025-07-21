e88449394b350b681e0540c8a16f7c98d0fd683e
Markus Mottl <markus.mottl@gmail.com>
2025-07-21 15:12:18 -0400

    Use longer snacks picker file paths

--- collections/mmottl/nvim/dot-config/nvim/lua/plugins/snacks.lua
+++ collections/mmottl/nvim/dot-config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,11 @@ return {
       },
     },
     picker = {
+      formatters = {
+        file = {
+          truncate = 60,
+        },
+      },
       sources = {
         explorer = {
           win = {
