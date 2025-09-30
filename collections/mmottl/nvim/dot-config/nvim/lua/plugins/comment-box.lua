2e4a8e91211efbfea2dbcdfded7cfcb22ddf3adb
Markus Mottl <markus.mottl@gmail.com>
2025-09-30 10:15:22 -0400

    Added gcb keybinding

--- collections/mmottl/nvim/dot-config/nvim/lua/plugins/comment-box.lua
+++ collections/mmottl/nvim/dot-config/nvim/lua/plugins/comment-box.lua
@@ -1,1 +1,9 @@
-return { "LudoPinelli/comment-box.nvim" }
+return {
+  "LudoPinelli/comment-box.nvim",
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "gcb", "<Cmd>CBccbox<CR>", desc = "Box Title", mode = "n" },
+    })
+  end,
+}
