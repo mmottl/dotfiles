4284e5675313363f79ca95b3e4b5c527addf778e
Markus Mottl <markus.mottl@gmail.com>
2026-03-14 20:58:54 -0400

    Do not make vars for MCPHub

--- collections/mmottl/nvim/dot-config/nvim/lua/plugins/codecompanion.lua
+++ collections/mmottl/nvim/dot-config/nvim/lua/plugins/codecompanion.lua
@@ -148,7 +148,7 @@ return {
           callback = "mcphub.extensions.codecompanion",
           opts = {
             show_result_in_chat = true, -- Show mcp tool results in chat
-            make_vars = true, -- Convert resources to #variables
+            make_vars = false, -- Convert resources to #variables
             make_slash_commands = true, -- Add prompts as /slash commands
           },
         },
